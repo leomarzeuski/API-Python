a93edbf525297d51926f4c2a9a7cb9c38342cf8b
Leonardo Marzeuski <102033896+leomarzeuski@users.noreply.github.com>
2024-02-11 17:00:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
 import requests
 
-# Parâmetros de exemplo que você gostaria de adicionar à URL
-parametros_url = {'param1': 'valor1', 'param2': 'valor2'}
+# URL base do serviço que assina o documento
+base_url = "https://lazy-erin-snail-gown.cyclic.app/sign"
 
-# URL do seu serviço Flask hospedado no Cyclic, com parâmetros de query string
-url_do_servico = 'https://lazy-erin-snail-gown.cyclic.app/sign'
-url_com_parametros = requests.Request('POST', url_do_servico, params=parametros_url).prepare().url
-
-# Substitua estes placeholders pelos valores reais
-pfx_file_url = 'https://seuservidor.com/caminho/para/seu/arquivo.pfx'
-pfx_password = 'senhaDoPFX'
-pdf_url = 'https://seuservidor.com/caminho/para/o/documento.pdf'
-
-# Preparando os dados para enviar na solicitação POST
-dados = {
-    'pfx_file_url': pfx_file_url,
-    'pfx_password': pfx_password,
-    'pdf_url': pdf_url
+# Parâmetros de query string que você quer enviar
+params = {
+    'pfx_file_url': 'https://6ca3c31cd443b8133af7226009438b05.cdn.bubble.io/f1690373284903x746653125219639200/22172649.pfx',
+    'pfx_password': '123456',
+    'pdf_url': 'https://6ca3c31cd443b8133af7226009438b05.cdn.bubble.io/f1691518862230x245229468668821700/Laudo-Lepanto-08-08-23.pdf'
 }
 
-# Realizando a solicitação POST
-response = requests.post(url_com_parametros, json=dados)
+# Monta a URL completa com os parâmetros de query string
+full_url = requests.Request('POST', base_url, params=params).prepare().url
 
-# Verificando a resposta
-if response.status_code == 200:
-    print("Sucesso:", response.json())
+# Enviar solicitação POST
+response = requests.post(full_url)
+
+# Verificar a resposta
+if response.ok:
+    print('Sucesso:', response.json())
+    # Aqui você pode adicionar código para manipular o documento retornado
 else:
-    print("Erro:", response.status_code, response.text)
+    print('Erro:', response.status_code, response.text)
